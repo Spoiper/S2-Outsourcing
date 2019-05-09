ae05c58ee41c2f36afcc7f1f360949182720cc78
Nick Litjens <litjens.nick@gmail.com>
2019-05-09 13:51:07 +0200

    Add project files.

---
+++ S2-Outsource-Project/wwwroot/js/Translate.js
@@ -0,0 +1,62 @@
+﻿// Json file ophalen
+var arrLang = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "json/Multilingual.json",
+        'dataType': "json",
+        'success': function (translations) {
+         json = translations;
+        }
+    });
+    return json;
+})();
+
+// Standaard Taal is de browsertaal of Engels.
+$(document).ready(function () {
+	var yetVisited = localStorage["visited"];
+
+    // Zorgt er voor dat de taal niet veranderd bij het navigeren op de website.
+    if (!yetVisited) {
+        var browserLang = navigator.language;
+        var displayLang = browserLang.split("-")[0];
+		var lang = displayLang;
+
+		// Wanneer de browser niet Nederlands is wordt de website Engels.
+		if (lang == "nl") {
+			localStorage.setItem("lang", lang);
+			$(".lang").each(function (index, element) {
+				$(this).text(arrLang[lang][$(this).attr("key")]);
+			});
+			localStorage["visited"] = "yes";
+		}
+
+		else
+		{
+			localStorage.setItem("lang", "en");
+			$(".lang").each(function (index, element) {
+				$(this).text(arrLang[lang][$(this).attr("key")]);
+			});
+			localStorage["visited"] = "yes";
+		}
+    }
+    else {
+        var lang = localStorage.getItem("lang");
+        $(".lang").each(function (index, element) { 
+            $(this).text(arrLang[lang][$(this).attr("key")]);
+        });
+    }
+});
+
+// get/set Taal
+$(".translate").click(function () {
+    localStorage.setItem("lang", $(this).attr("id"));
+    var lang = localStorage.getItem("lang");
+
+    $(".lang").each(function (index, element) {
+        $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+});
+
+
---
+++ S2-Outsource-Project2/S2-Outsource-Project2/Controllers/TranslateController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace S2_Outsource_Project2.Controllers
+{
+    public class TranslateController : Controller
+    {
+	    public IActionResult Index()
+	    {
+		    ViewBag.Test = "hallo";
+		    ViewBag.Test2 = "groen";
+		    ViewBag.Test3 = "kaas";
+			return View();
+	    }
+
+
+		public IActionResult TranslateEn()
+		{
+			ViewBag.Test = "hello";
+			ViewBag.Test2 = "green";
+			ViewBag.Test3 = "cheese";
+			return View("Index");
+		}
+	}
+}
